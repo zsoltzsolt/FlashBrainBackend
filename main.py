50babb1be62f70e42b24a7cbdf2d5d8f5e8e683d
Diriczi Zsolt-Csaba <z.diriczi@yahoo.com>
2023-11-25 21:20:41 +0200

    Add database model for user entity

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from db import models
+from db.database import engine
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+models.Base.metadata.create_all(engine)
