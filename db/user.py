4488ecb463cd16f78c9f83f2165d410d16310a3c
Diriczi Zsolt-Csaba <z.diriczi@yahoo.com>
2023-11-25 21:52:48 +0200

    Add function to send email verification

--- db/user.py
+++ db/user.py
@@ -3,6 +3,8 @@ from routers.schemas import UserBase, UserDisplay
 from db.models import DbUser
 from db.database import SessionLocal
 from db.hashing import Hash
+from email1.emailConfirmationData import create_subject_body
+from email1.emailSender import send_email
 
 def create_user_func(db: Session, request: UserBase):
     new_user = DbUser(
@@ -13,6 +15,8 @@ def create_user_func(db: Session, request: UserBase):
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
+    subject, body = create_subject_body(request.username, "https://example.com")
+    send_email(request.email, subject, body)
     return new_user
 
 def get_user_by_username(db: Session, username: str):
---
+++ email1/emailSender.py
@@ -0,0 +1,26 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def send_email(receiver_email, subject, message_body):
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587  
+    
+    sender_email = "zsoltzsolt303@gmail.com"  
+    sender_password = "ukhr jrny uehq mxzj"
+
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()  
+
+    server.login(sender_email, sender_password)
+
+    msg = MIMEMultipart()
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+    msg["Subject"] = subject
+
+    msg.attach(MIMEText(message_body, "plain"))
+
+    server.sendmail(sender_email, receiver_email, msg.as_string())
+
+    server.quit()
