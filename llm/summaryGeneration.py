a51a64c46c980aa96cb1c53971402b0fd6cc9992
Diriczi Zsolt-Csaba <z.diriczi@yahoo.com>
2023-12-13 15:25:01 +0200

    Add function to generate metadata about a local PDF file

--- llm/summaryGeneration.py
+++ llm/summaryGeneration.py
@@ -7,6 +7,23 @@ from langchain.chains.llm import LLMChain
 from langchain.prompts import PromptTemplate
 from config import OPENAI_API_KEY
 
+def generate_metadata(path: str) -> dict:
+    loader = PyPDFLoader(path)
+    docs = loader.load()
+
+    prompt_template = """Return a this information: 'title' and 'category' in json format where 'title' represents a sugestive title for the following
+    text and 'category' spcifies the number coresponding to the correct category: 2 (math), 3 (science), 4 (history), 5 (languages), 6 (computer science), 7 (geography), 8 (economics), 9 (others). 
+    "{text}" """
+    prompt = PromptTemplate.from_template(prompt_template)
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k", api_key=OPENAI_API_KEY)
+    llm_chain = LLMChain(llm=llm, prompt=prompt)
+
+    stuff_chain = StuffDocumentsChain(llm_chain=llm_chain, document_variable_name="text")
+
+    docs = loader.load()
+    return stuff_chain.run(docs)
+
 def generate_summaries(path: str) -> str:
     loader = PyPDFLoader(path)
     docs = loader.load()
