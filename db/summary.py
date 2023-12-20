54d377268e9152a7d6183ccbb8f2fe661e8ba793
Diriczi Zsolt-Csaba <z.diriczi@yahoo.com>
2023-12-20 19:29:25 +0200

    Add endpoint to delete summaries

--- db/summary.py
+++ db/summary.py
@@ -1,5 +1,21 @@
 from sqlalchemy.orm import Session
 from db.models import DbSummary
+from routers.schemas import UserDisplay
+from db.models import DbSummary
+from sqlalchemy import and_
 
 def get_all(db: Session):
     return db.query(DbSummary).all()
+
+def delete_summary(summaryId: int, db: Session, user: UserDisplay):
+    like = db.query(DbSummary).filter(
+        and_(
+            summaryId == DbSummary.summaryId, 
+            user.uid == DbSummary.ownerId)
+        ).first()
+    if like:
+        db.delete(like)
+        db.commit()
+        return "ok"
+    return None
+    
--- routers/summary.py
+++ routers/summary.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter
-from db.summary import get_all
+from db.summary import get_all, delete_summary
 from db.database import get_db
 from sqlalchemy.orm.session import Session
 from fastapi import Depends
 from routers.schemas import SummaryDisplay
 from typing import List
+from routers.schemas import UserDisplay
+from auth.oauth2 import get_current_active_user
+from fastapi.exceptions import HTTPException
+from fastapi import status
 
 router = APIRouter(
     prefix="/summary",
@@ -14,3 +18,10 @@ router = APIRouter(
 @router.get("/all", response_model=List[SummaryDisplay])
 def get_all_summaries(db: Session = Depends(get_db)):
     return get_all(db)
+
+@router.delete("/{summaryId}")
+def delete_summary1(summaryId: int, db: Session = Depends(get_db), user: UserDisplay = Depends(get_current_active_user)):
+    if delete_summary(summaryId, db, user) is None:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"Summary with id {summaryId} not found!")
+    else:
+        return HTTPException(status_code=status.HTTP_200_OK, detail = f"Summary with id {summaryId} deleted succesfully!")
