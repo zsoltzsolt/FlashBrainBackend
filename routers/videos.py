68034e8141fa9bb8332cba28cc6a328a0c34dece
Diriczi Zsolt-Csaba <z.diriczi@yahoo.com>
2023-12-22 12:04:05 +0200

    Add BackgroundTask for video summary generation

--- routers/videos.py
+++ routers/videos.py
@@ -9,6 +9,7 @@ from auth.oauth2 import get_current_active_user
 from email1.emailSender import send_email
 from email1.summaryReady import create_subject_body
 from time import sleep
+from fastapi import BackgroundTasks
 
 router = APIRouter(
     prefix="/summary/video",
@@ -18,26 +19,31 @@ router = APIRouter(
 def getIdFromUrl(url):
     return url.split("/")[-1]
 
-@router.post("/")
-def getVideo(
-    youtube: YouTubeBase,
-    request: SummarySourceBase = Depends(),
-    db: Session = Depends(get_db),
-    user: UserDisplay = Depends(get_current_active_user)
-):
-    # We get the url but we need to get the id
-     
+def create_summary( youtube: YouTubeBase,request: SummarySourceBase,db: Session,user: UserDisplay):
+    
     summary = SummaryBase(title="", 
                           ownerId=user.uid, 
                           categoryId=1, 
                           isPublic=request.isPublic, 
                           path=youtube.url)
+    
     id1 = YoutubeSummaryGenerator().generate_summary(summary, db)
     
-    sleep(300)
+    sleep(100)
     
     subject, body = create_subject_body(user.username, f"localhost:3000/summaries/{id1}")
     
     send_email(user.email, subject, body)
     
-    return {"response": "Summary generated succesfully!"}
+
+@router.post("/")
+async def getVideo(
+    background_tasks: BackgroundTasks,
+    youtube: YouTubeBase,
+    request: SummarySourceBase = Depends(),
+    db: Session = Depends(get_db),
+    user: UserDisplay = Depends(get_current_active_user)
+):
+    background_tasks.add_task(create_summary, youtube, request, db, user)
+    return {"message": "Merry Christmas Radu"}
+
